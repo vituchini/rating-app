5eaedc3e6fdb3c1d953817adb15f3e77d046411e
vituchini <petrovichvictorpro99@gmail.com>
2022-10-06 14:40:50 +0300

    Add context

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { SidebarProps } from './Sidebar.props'
+import { Menu } from '../Menu/Menu'
+
 import styles from './Sidebar.module.scss'
 
 export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
-  return <div {...props}>Sidebar</div>
+  return (
+    <div {...props}>
+      <Menu />
+    </div>
+  )
 }
