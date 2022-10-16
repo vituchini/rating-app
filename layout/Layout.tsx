29e94ee88b7bfefc9e3442dddd82f2eafa87cfea
vituchini <69416374+vituchini@users.noreply.github.com>
2022-10-16 18:20:56 +0300

    Create menu accessibility (#15)

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react'
-import { AppContextProvider, IAppContext } from '../context/app.context'
+import React, { FC, useState, KeyboardEvent, useRef } from 'react'
+import clsx from 'clsx'
 
+import { AppContextProvider, IAppContext } from '../context/app.context'
 import { LayoutProps } from './Layout.props'
 import { Header } from './Header/Header'
 import { Sidebar } from './Sidebar/Sidebar'
@@ -9,11 +10,34 @@ import { Up } from '../components'
 import styles from './Layout.module.scss'
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
+  const [isSkipLink, setIsSkipLink] = useState<boolean>(false)
+  const bodyRef = useRef<HTMLDivElement>(null)
+
+  const skipContentAction = (keyEvent: KeyboardEvent) => {
+    if (keyEvent.code === 'Space' || keyEvent.code === 'Enter') {
+      keyEvent.preventDefault()
+      bodyRef.current.focus()
+    }
+    setIsSkipLink(false)
+  }
+
   return (
     <div className={styles.wrapper}>
+      <a
+        onFocus={() => setIsSkipLink(true)}
+        tabIndex={1}
+        onKeyDown={skipContentAction}
+        className={clsx(styles.skipLink, {
+          [styles.displayedLink]: isSkipLink,
+        })}
+      >
+        Сразу к содержанию
+      </a>
       <Header className={styles.header} />
       <Sidebar className={styles.sidebar} />
-      <div className={styles.body}>{children}</div>
+      <div ref={bodyRef} tabIndex={0} className={styles.body}>
+        {children}
+      </div>
       <Footer className={styles.footer} />
       <Up />
     </div>
